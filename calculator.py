0d1ec8ae860155d6e320d0e3873756410a6946a0
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-03-10 09:25:39 +0100

    added new file

---
+++ calculator.py
@@ -0,0 +1,33 @@
+# a + b = c
+# Отримати a
+# Отримати b
+# Отримати + | - | / | *
+
+# Calculator
+# a = int(input('Enter a: '))
+# b = int(input("Enter b:  "))
+
+# oper = input('Enter operator: ')
+
+# if oper == "+":
+#     c = a + b
+# elif oper == "-":
+#     c = a - b
+# elif oper == "*":
+#     c = a * b
+# elif oper == "/":
+#     c = a / b 
+# else:
+#     print("Incorect operator")
+#     exit(1)
+# print(f"Result is: {c}")
+
+
+
+
+# length = input("Enter length: ")
+# width = input("Enter width ")
+# area = float(length) * float(width)
+
+age = input("How old are you? ")
+print(type(age))
