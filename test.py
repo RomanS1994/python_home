95b38cbb1b83fd9ece981543f55ae1f308b876a9
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-03-11 22:29:49 +0100

    add new practice

--- test.py
+++ test.py
@@ -8,6 +8,81 @@
 # print(b)
 
 
-x = 1 
-x += 4
-print(x)
+# x = 1 
+# x += 4
+# print(x)
+
+######## Модуль 2 ######
+
+#### №1
+# name = input("What is your name? ")
+# print(f"Hello {name}!!!")
+
+
+#### №2
+
+# age = input("How old are you? ")
+# if int(age) >= 18:
+#     print(111)
+# else: 
+#     print(222)
+
+#### №3
+
+# a = int(input("Введіть число"))
+# if a == 1:
+#     print(1)
+# elif a == 2:
+#     print(2)
+# elif a == 3:
+#     print(3)
+# elif a == 4:
+#     print(4)
+# else:
+#     print("number")
+
+### №4
+
+# x = int(input("X: "))
+# y = int(input("Y: "))
+
+# if x == 0:
+#      print("X can`t be equal to zero")
+#      x = int(input("X: "))
+
+#     if x == 0:
+#         print("X can`t be equal to zero")
+#         x = int(input("X: "))
+
+#         if x == 0:
+#             print("X can`t be equal to zero")
+#             x = int(input("X: "))
+
+# result = y / x
+
+### №4 for in
+# a = 'Apple'
+# for b in a:
+#     print(b)
+
+### №5 while 
+
+# a = 1
+# while a < 5:
+#     print(a)
+#     a += 1
+#############################
+
+# while True:
+#     number = input("number = ")
+#     number = int(number)
+#     while True:
+#         print(number)
+#         number = number - 1
+#         if number < 0:
+#             break
+
+#-------------------------------
+#Вийнятки
+
+int("a")
