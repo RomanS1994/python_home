5a569020d5a6e85a1ff95d487c042b482e6cdca7
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-04-09 22:10:31 +0200

    added new tasck

--- home_work_3.py
+++ home_work_3.py
@@ -1,11 +1,24 @@
-base_rate = 40
-price_per_km = 10
-total_trip = 0
+# base_rate = 40
+# price_per_km = 10
+# total_trip = 0
 
-def calculate_trip_price(distance_km):
-    global total_trip  
-    total_trip += 1
-    return base_rate + distance_km * price_per_km
+# def calculate_trip_price(distance_km):
+#     global total_trip  
+#     total_trip += 1
+#     return base_rate + distance_km * price_per_km
 
-print(calculate_trip_price(10))
-print("Total trips:", total_trip)
+# print(calculate_trip_price(10))
+# print("Total trips:", total_trip)
+
+#----------- Number 6---------------------
+def format_string(string, length):
+    if len(string) >= length:
+        return string
+    else:
+        spaces_to_add = (length - len(string)) // 2
+        formatted_string = ' ' * spaces_to_add + string
+        return formatted_string
+
+# Приклади використання функції для тестів
+print(format_string('aaaaaaaaaaaaaaaaac', 12))
+print(format_string('abaa', 15))
