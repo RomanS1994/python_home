f3b125012d834d04af93ef39c796ab65789de614
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-09-21 22:00:34 +0200

    add new file

--- New Python/lesson_4.py
+++ New Python/lesson_4.py
@@ -13,3 +13,35 @@
 ##### -------- Методи рядків
 
 ### find()
+# str = "Hello World"
+#
+# print(str.find("lo", 1, 8))
+
+
+##### ------- Поділ та об'єднання рядків
+## --- Поділ
+# text = "apple, banana, cherry"
+# result = text.split()
+# print(result)
+
+## --- об'єднання
+
+# list = ["hello", "world"]
+# result = ', hi '.join(list)
+# print(result)
+
+# str = "Hello @ World"
+# test,_ = str.split("@")
+# print(test)
+
+url_search = "<https://www.google.com/search?q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t>"
+_, query = url_search.split('?')
+print(query)
+
+obj_query = {}
+for el in query.split('&'):
+    key, value = el.split('=')
+    obj_query.update({key: value.replace('+', ' ')})
+print(obj_query)
+
+
