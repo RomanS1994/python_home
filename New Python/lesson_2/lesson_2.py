dc8448ef189e22e2148058cfb14f6fd456d5ed1a
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-09-27 10:05:41 +0200

    update lessonpython

---
+++ New Python/lesson_2/lesson_2.py
@@ -0,0 +1,40 @@
+# fruits = ['apple', 'banana', 'count', 'pineaple']
+# print(fruits[1:])
+# print(fruits.pop(1))
+# print(dir(fruits))
+
+# fruits = ['apple', 'banana', 'count', 'pineaple']
+# print(dir(fruits))
+
+# a, b, c, P, p/2, s = p(p-a)*(p-b)*(p-c)
+
+# a = 3
+# b = 4
+# c = 5
+
+# age = int(input('how old are you >>> '))
+
+# if age > 18:
+#     print('you can have a beet')
+    
+
+# a = int(input('a >>> '))
+# b = int(input('b >>> '))
+# print(min(a, b))
+
+
+
+# Попрактикувати зрізи
+
+# Зробити задачу на if
+
+username = input('enter your username please')
+password = input('enter your password please')
+
+if username == 'admin' and password.isalnum:
+
+
+
+
+
+
